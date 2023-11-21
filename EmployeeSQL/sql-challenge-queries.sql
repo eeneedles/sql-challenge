c7b767cc8189928e26a15e1c1dd7e3adb4b79b89
eeneedles <132238481+eeneedles@users.noreply.github.com>
2023-11-20 23:23:06 -0500

    updated sql-challenge-queries

--- EmployeeSQL/sql-challenge-queries.sql
+++ EmployeeSQL/sql-challenge-queries.sql
@@ -22,18 +22,19 @@ FROM Dept_manager AS dm
 		JOIN Employees AS e
 		ON (dm.emp_no = e.emp_no)
 		GROUP BY dm.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name;
-
-
-
+		
+		
 --List the department number for each employee along with that employee’s employee 
 --number, last name, first name, and department name.
 SELECT dept_no, emp_no, last_name, first_name, dept_name
 FROM
 
+
 --List first name, last name, and sex of each employee whose first name is Hercules
 --and whose last name begins with the letter B.
 SELECT first_name, last_name, sex FROM Employees
 WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
+
 
 --List each employee in the Sales department, including their employee number,
 --last name, and first name.
@@ -57,6 +58,7 @@ FROM Employees AS e
 		ON(de.dept_no = d.dept_no)
 			WHERE d.dept_name = 'd007' OR d.dept_no = 'd005'
 			GROUP BY d.dept_name, de.emp_no, de.dept_no, e.last_name, e.first_name;
+
 
 --List the frequency counts, in descending order, of all the employee last names
 --(that is, how many employees share each last name).
